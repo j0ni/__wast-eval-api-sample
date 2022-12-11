2e29555b18b3c31f2b678914d216cd0db869832e
Anatolii Smolianinov <zarkonesmall@gmail.com>
2022-12-11 12:04:24 +0100

    /eval init

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 // use wasmer::FunctionEnv;
+use actix_web::{error, web, App, HttpResponse, HttpServer, Responder};
+use serde::Deserialize;
 use wasmer::{imports, Instance, Module, Store, Value};
 
-fn eval_program(wast: &str) -> anyhow::Result<i32> {
+#[derive(Deserialize)]
+struct EvalBody {
+    wast: String,
+}
+
+fn eval_program(wast: String) -> anyhow::Result<i32> {
     let mut store = Store::default();
     let module = Module::new(&store, &wast)?;
     // The module doesn't import anything, so we create an empty import object.
@@ -17,21 +24,44 @@ fn eval_program(wast: &str) -> anyhow::Result<i32> {
     }
 }
 
-fn main() -> anyhow::Result<()> {
-    let wast = r#"
-    (module
-      (type $t0 (func (param i32) (result i32)))
-      (func $add_one (export "add_one") (type $t0) (param $p0 i32) (result i32)
-        get_local $p0
-        i32.const 1
-        i32.add))
-    "#;
+async fn eval_handler(eval_body: web::Json<EvalBody>) -> impl Responder {
+    format!("Welcome {}!", eval_body.wast)
+}
 
-    let res = eval_program(wast);
-    match res {
-        Ok(val) => println!("{}", val),
-        Err(e) => println!("Error: {}", e),
-    }
+// async fn eval_handler(req: HttpRequest) -> impl Responder {
+//     let wast = r#"
+//     (module
+//       (type $t0 (func (param i32) (result i32)))
+//       (func $add_one (export "add_one") (type $t0) (param $p0 i32) (result i32)
+//         get_local $p0
+//         i32.const 1
+//         i32.add))
+//     "#;
 
-    Ok(())
+//     let res = eval_program(wast);
+//     match res {
+//         Ok(val) => println!("{}", val),
+//         Err(e) => println!("Error: {}", e),
+//     }
+// }
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        let json_config = web::JsonConfig::default().error_handler(|err, _req| {
+            // create custom error response
+            println!("{}", err.to_string());
+            error::InternalError::from_response(err, HttpResponse::Conflict().finish()).into()
+        });
+
+        App::new().service(
+            web::resource("/eval")
+                // change json extractor configuration
+                .app_data(json_config)
+                .route(web::post().to(eval_handler)),
+        )
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
